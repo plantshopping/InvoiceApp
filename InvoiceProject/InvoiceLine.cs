ff794dd36529c1a2b2ada2fb9b615ddf89f28ce7
Amy Lin <xiaohui@outlook.co.nz>
2020-10-11 16:37:06 +1300

    Update cost to have decimal type

--- InvoiceProject/InvoiceLine.cs
+++ InvoiceProject/InvoiceLine.cs
@@ -5,6 +5,6 @@
         public int InvoiceLineId { get; set; }
         public string Description { get; set; }
         public int Quantity { get; set; }
-        public double Cost { get; set; }
+        public decimal Cost { get; set; }
     }
 }
