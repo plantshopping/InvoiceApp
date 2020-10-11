e6c77f3bf60e74dccc8d44695fbff2fd8061baa9
Amy Lin <xiaohui@outlook.co.nz>
2020-10-11 22:00:35 +1300

    Refactor

--- Source/Xero.InvoiceApp.Core/Models/Invoice.cs
+++ Source/Xero.InvoiceApp.Core/Models/Invoice.cs
@@ -12,40 +12,26 @@ namespace InvoiceProject
         public List<InvoiceLine> LineItems { get; set; } = new List<InvoiceLine>();
         public decimal Total => LineItems.Sum(l => l.TotalCost);
 
-        public void AddInvoiceLine(InvoiceLine invoiceLine)
-        {
-            LineItems.Add(invoiceLine);
-        }
+        public void AddInvoiceLine(InvoiceLine invoiceLine) => LineItems.Add(invoiceLine);
 
-        public void RemoveInvoiceLine(int id)
-        {
-            LineItems.RemoveAll(l => l.InvoiceLineId == id);
-        }
-
+        public void RemoveInvoiceLine(int id) => LineItems.RemoveAll(l => l.InvoiceLineId == id);
+        
         /// <summary>
         /// MergeInvoices appends the items from the sourceInvoice to the current invoice
         /// </summary>
         /// <param name="sourceInvoice">Invoice to merge from</param>
-        public void MergeInvoices(Invoice sourceInvoice)
-        {
-            LineItems.AddRange(sourceInvoice.LineItems);
-        }
-
+        public void MergeInvoices(Invoice sourceInvoice) => LineItems.AddRange(sourceInvoice.LineItems);
+        
         /// <summary>
         /// Creates a deep clone of the current invoice (all fields and properties)
         /// </summary>
-        public Invoice Clone()
-        {
-            return this.DeepClone();
-        }
+        public Invoice Clone() => this.DeepClone();
 
         /// <summary>
         /// Outputs string containing the following (replace [] with actual values):
         /// Invoice Number: [InvoiceNumber], InvoiceDate: [dd/MM/yyyy], LineItemCount: [Number of items in LineItems]
         /// </summary>
-        public override string ToString()
-        {
-            return string.Format(Strings.InvoiceTemplate, InvoiceNumber.ToString(), InvoiceDate.ToString("dd/MM/yyyy"), LineItems.Count);
-        }
+        public override string ToString() => string.Format(Strings.InvoiceTemplate, InvoiceNumber.ToString(), InvoiceDate.ToString("dd/MM/yyyy"), LineItems.Count);
+        
     }
 }
